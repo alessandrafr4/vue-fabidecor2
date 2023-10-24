00e713bb4025b355dca3d7b7787a1ffb2ec61b0c
alessandrafr4 <alessandralelehfr495@gmail.com>
2023-10-24 14:58:29 -0300

    24 de outubro

--- src/plugins/api.js
+++ src/plugins/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://191.52.55.168:19003/api/',
+  // baseURL: 'http://191.52.55.168:19003/api/',
+  baseURL: 'https://fabidecor-dev-bqpz.2.ie-1.fl0.io/api/',
 })
 
 export default api
